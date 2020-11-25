ab3a3c1590993e06340f8db5c5cae8d6bf0f6555
Stéphane Dreyfus <floatingrock@gmail.com>
2020-11-25 01:10:57 -0800

    Add default props. Why is zeroCount prop value changing?

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,41 +4,14 @@ import useAsyncReference from './helpers';
 import ColorCount from './ColorCount';
 import './EightBall.css';
 
-const initial = {msg: "Think of a question.", color: "black"};
-
-const answers = [
-  { msg: "It is certain.", color: "green" },
-  { msg: "It is decidedly so.", color: "green" },
-  { msg: "Without a doubt.", color: "green" },
-  { msg: "Yes - definitely.", color: "green" },
-  { msg: "You may rely on it.", color: "green" },
-  { msg: "As I see it, yes.", color: "green" },
-  { msg: "Most likely.", color: "green" },
-  { msg: "Outlook good.", color: "green" },
-  { msg: "Yes.", color: "green" },
-  { msg: "Signs point to yes.", color: "goldenrod" },
-  { msg: "Reply hazy, try again.", color: "goldenrod" },
-  { msg: "Ask again later.", color: "goldenrod" },
-  { msg: "Better not tell you now.", color: "goldenrod" },
-  { msg: "Cannot predict now.", color: "goldenrod" },
-  { msg: "Concentrate and ask again.", color: "goldenrod" },
-  { msg: "Don't count on it.", color: "red" },
-  { msg: "My reply is no.", color: "red" },
-  { msg: "My sources say no.", color: "red" },
-  { msg: "Outlook not so good.", color: "red" },
-  { msg: "Very doubtful.", color: "red" },
-];
-
-const zeroCount = {green: 0, golden: 0, red: 0};
-
-
-const EightBall = () => {
+const EightBall = ({ answers, initial, zeroCount }) => {
   const [info, setInfo] = useAsyncReference(initial);
   const [count, setCount] = useAsyncReference(zeroCount);
   
   // FIX ME: Due to state timing, color count is off.
   const handleClick = () => {
-
+    console.log("What is zerocount?", zeroCount);
+    console.log("What is initial?", initial);
     setInfo(answers[Math.floor(Math.random() * answers.length)]);
 
     if (info.current.color === "red") {
@@ -70,5 +43,32 @@ const EightBall = () => {
     </div>
   );
 };
+
+EightBall.defaultProps = {
+  answers: [
+    { msg: "It is certain.", color: "green" },
+    { msg: "It is decidedly so.", color: "green" },
+    { msg: "Without a doubt.", color: "green" },
+    { msg: "Yes - definitely.", color: "green" },
+    { msg: "You may rely on it.", color: "green" },
+    { msg: "As I see it, yes.", color: "green" },
+    { msg: "Most likely.", color: "green" },
+    { msg: "Outlook good.", color: "green" },
+    { msg: "Yes.", color: "green" },
+    { msg: "Signs point to yes.", color: "goldenrod" },
+    { msg: "Reply hazy, try again.", color: "goldenrod" },
+    { msg: "Ask again later.", color: "goldenrod" },
+    { msg: "Better not tell you now.", color: "goldenrod" },
+    { msg: "Cannot predict now.", color: "goldenrod" },
+    { msg: "Concentrate and ask again.", color: "goldenrod" },
+    { msg: "Don't count on it.", color: "red" },
+    { msg: "My reply is no.", color: "red" },
+    { msg: "My sources say no.", color: "red" },
+    { msg: "Outlook not so good.", color: "red" },
+    { msg: "Very doubtful.", color: "red" },
+  ],
+  initial: {msg: "Think of a question.", color: "black"},
+  zeroCount: {green: 0, golden: 0, red: 0}
+}
 
 export default EightBall;
